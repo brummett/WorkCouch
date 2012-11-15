7c2f49408d6133744e1f2aff739c4ce8966fe73a
Anthony Brummett <abrummet@genome.wustl.edu>
2012-11-15 16:27:50 -0600

    comment out prints

--- job-runner.pl
+++ job-runner.pl
@@ -12,7 +12,7 @@ my $server = WorkflowComms->new($uri);
 
 my $job_id = $ARGV[0];
 
-print STDERR "job runner for job $job_id\n";
+#print STDERR "job runner for job $job_id\n";
 
 my $job = $server->_get_doc($job_id);
 my $command = $job->{'cmdline'};
@@ -21,36 +21,42 @@ my $job_start_time = Time::HiRes::time();
 my $pid = fork();
 if (defined($pid) && !$pid) {
     # child
-    print STDERR "Job runner about to exec $command\n";
+    #print STDERR "Job runner about to exec $command\n";
     exec($command);
     exit(1);
 }
 
 # parent
-print STDERR "Job runner child pid is $pid\n";
+#print STDERR "Job runner child pid is $pid\n";
+
 my $result = $server->job_is_running($job_id, $pid);
 if ($result ne 'success') {
     die "Couldn't set job $job_id running: $result";
 }
 
-print STDERR "Job runner waiting for child $pid to exit\n";
+#print STDERR "Job runner waiting for child $pid to exit\n";
 waitpid($pid, 0);
+#print STDERR "Job finished: $?\n";
+
 my $job_wall_time = Time::HiRes::time() - $job_start_time;
 my($user,$system,$cuser,$csystem) = times();
 
-my $exit_code = $? >> 8;
-print STDERR "Job runner child $pid is done! exit code $exit_code\n";
-my $signal = $? & 127;
-my $coredump = $? & 128;
+my $exit_code = ($? >> 8);
+#print STDERR "Job runner child $pid is done! exit code $exit_code\n";
+my $signal = ($? & 127);
+my $coredump = ($? & 128);
 
 my %stats = (   result => $exit_code,
                 cpuTime => ($cuser+$csystem),
                 wallTime => $job_wall_time );
+#print "Job stats: ".Data::Dumper::Dumper(\%stats);
 
 if ($exit_code) {
     # crashed!
+#print STDERR "Jobs crashed!\n";
     $server->job_is_crashed($job_id, %stats, signal => $signal, coredump => $coredump);
 } else {
+#print STDERR "Job is done!\n";
     $server->job_is_done($job_id, %stats);
 }
 
