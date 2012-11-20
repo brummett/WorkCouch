697094c8a6bbe70dbeaa139b031b29b270357d5e
Anthony Brummett <abrummet@genome.wustl.edu>
2012-11-20 17:32:19 -0600

    Make the parallel job submitter more efficient by sending fewer queries

--- submit-parallel-jobs.pl
+++ submit-parallel-jobs.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
 
+use strict;
+use warnings;
+
 use WorkflowComms;
 
 my $count = $ARGV[0] - 2;  # subtract the starting and ending nodes
@@ -7,61 +10,38 @@ my $count = $ARGV[0] - 2;  # subtract the starting and ending nodes
 my $server = WorkflowComms->new('http://localhost:5985/workflow/');
 
 # make a starting node
-#my $starting_id = $server->enqueue({
-#        workflowId  => 1,
-#        clusterId   => 1,
-#        queueId     => 'short',
-#        label       => 'sleep',
-#        cmdline     => '/bin/sleep 0',
-#        #depends     => undef
-#        waitinOn    => 0,
-#    });
-
 my $starting_node = {
     workflowId  => 1,
         clusterId   => 1,
         queueId     => 'short',
-        label       => 'sleep',
+        label       => 'start',
         cmdline     => '/bin/sleep 0',
         dependants  => [],
         waitinOn    => 0,
     };
 
 
-#my @middle_ids;
 my @middle_nodes;
 foreach my $i ( 1 .. $count ) {
-#    my $id = $server->enqueue({
-#        workflowId  => 1,
-#        clusterId   => 1,
-#        queueId     => 'short',
-#        label       => 'sleep',
-#        cmdline     => '/bin/sleep 0',
-#        waitingOn   => 1,
-#    });
-#    push @middle_ids, $id;
     push @middle_nodes, {
         workflowId  => 1,
         clusterId   => 1,
         queueId     => 'short',
-        label       => 'sleep',
+        label       => 'middle',
         cmdline     => '/bin/sleep 0',
         waitingOn   => 1,
     };
 }
-
-#$server->add_dependant($starting_id, $_) foreach @middle_ids;
 
 # and an ending node
 my $ending_id = $server->enqueue({
         workflowId  => 1,
         clusterId   => 1,
         queueId     => 'short',
-        label       => 'sleep',
+        label       => 'end',
         cmdline     => '/bin/sleep 0',
-        waitingOn   => scalar(@middle_ids),
+        waitingOn   => scalar(@middle_nodes),
     });
-#$server->add_dependant($_, $ending_id) foreach @middle_ids;
 
 my @middle_ids;
 foreach (@middle_nodes) {
@@ -70,5 +50,5 @@ foreach (@middle_nodes) {
     push @middle_ids, $id;
 }
 
-$stating_node->{'dependants'} = \@middle_ids;
+$starting_node->{'dependants'} = \@middle_ids;
 $server->enqueue($starting_node);
